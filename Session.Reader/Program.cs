b3a2ce31cd070bd3bc0e3ff8ae0c7826626b8c00
Anish Aravind <anish.arvind407@gmail.com>
2020-03-01 08:07:13 +0530

    sessions

---
+++ Session.Reader/Program.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Management;
+using Newtonsoft.Json;
+using RfidCheckout.Config;
+using RfidCheckout.Messages;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Session.Reader
+{
+    class Program
+    {
+            static async Task Main(string[] args)
+        {
+            Console.WriteLine("Checkout Console");
+
+            // Create a new management client
+            var managementClient = new ManagementClient(AccountDetails.ConnectionString);
+
+            // Delete the queue if it exists.
+            if (await managementClient.QueueExistsAsync(AccountDetails.QueueName))
+            {
+                await managementClient.DeleteQueueAsync(AccountDetails.QueueName);
+            }
+
+            // Create a description for the queue.
+            QueueDescription rfidCheckoutQueueDescription =
+                new QueueDescription(AccountDetails.QueueName)
+                {
+                    //require sessions
+                    RequiresSession = true
+                };
+
+            // Create a queue based on the queue description.
+            await managementClient.CreateQueueAsync(rfidCheckoutQueueDescription);
+
+            // Create a session client
+            var sessionClient = new SessionClient
+                (AccountDetails.ConnectionString, AccountDetails.QueueName);
+
+            //executes once for each session
+            while (true)
+            {
+                Console.WriteLine("Accepting a message session...");
+                Console.ForegroundColor = ConsoleColor.White;
+
+                // Accept a message session                    
+                var messageSession = await sessionClient.AcceptMessageSessionAsync();
+
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                Console.WriteLine($"Accepted session: { messageSession.SessionId }");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+
+                int receivedCount = 0;
+                double billTotal = 0.0;
+
+                while (true)
+                {
+                    // Receive a message and set timespan.. if no message is received in 
+                    //1 sec then we close the session.
+                    var message = await messageSession.ReceiveAsync(TimeSpan.FromSeconds(1));
+
+                    if (message == null)
+                    {
+                        Console.WriteLine("Closing session...!");
+                        await messageSession.CloseAsync();
+                        break;
+                    }
+                    else
+                    {
+                        // Process the order message
+                        var rfidJson = Encoding.UTF8.GetString(message.Body);
+                        var rfidTag = JsonConvert.DeserializeObject<RfidTag>(rfidJson);
+                        Console.WriteLine($"{ rfidTag.ToString() }");
+
+                        receivedCount++;
+                        billTotal += rfidTag.Price;
+
+                        // Complete the message receive opperation
+                        await messageSession.CompleteAsync(message.SystemProperties.LockToken);
+                    }
+                }
+                // Bill the customer.
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine
+                    ("Bill customer ${0} for {1} items.", billTotal, receivedCount);
+            }
+        }
+
+    }
+}
---
+++ Session.Sender/Program.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.Azure.ServiceBus;
+using Newtonsoft.Json;
+using RfidCheckout.Config;
+using RfidCheckout.Messages;
+using System;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Session.Sender
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Tag Reader Console");
+
+            QueueClient queueClient = new QueueClient(AccountDetails.ConnectionString, AccountDetails.QueueName);
+
+
+            // Create a sample order
+            RfidTag[] orderItems = new RfidTag[]
+            {
+                    new RfidTag() { Product = "Ball", Price = 4.99 },
+                    new RfidTag() { Product = "Whistle", Price = 1.95 },
+                    new RfidTag() { Product = "Bat", Price = 12.99 },
+                    new RfidTag() { Product = "Bat", Price = 12.99 },
+                    new RfidTag() { Product = "Gloves", Price = 7.99 },
+                    new RfidTag() { Product = "Gloves", Price = 7.99 },
+                    new RfidTag() { Product = "Cap", Price = 9.99 },
+                    new RfidTag() { Product = "Cap", Price = 9.99 },
+                    new RfidTag() { Product = "Shirt", Price = 14.99 },
+                    new RfidTag() { Product = "Shirt", Price = 14.99 },
+            };
+
+            // Display the order data.
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Order contains {0} items.", orderItems.Length);
+            Console.ForegroundColor = ConsoleColor.Yellow;
+
+            double orderTotal = 0.0;
+            foreach (RfidTag tag in orderItems)
+            {
+                Console.WriteLine("{0} - ${1}", tag.Product, tag.Price);
+                orderTotal += tag.Price;
+            }
+
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Order value = ${0}.", orderTotal);
+            Console.WriteLine();
+            Console.ResetColor();
+
+            Console.WriteLine("Press enter to scan...");
+            Console.ReadLine();
+
+            Random random = new Random(DateTime.Now.Millisecond);
+
+
+            // Send the order with random duplicate tag reads
+            int sentCount = 0;
+            int position = 0;
+
+            Console.WriteLine("Reading tags...");
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            // Comment in to create session id
+            var sessionId = Guid.NewGuid().ToString();
+            Console.WriteLine($"SessionId: { sessionId }");
+
+            while (position < 10)
+            {
+                RfidTag rfidTag = orderItems[position];
+
+                // Create a new  message from the order item RFID tag.
+                var orderJson = JsonConvert.SerializeObject(rfidTag);
+                var tagReadMessage = new Message(Encoding.UTF8.GetBytes(orderJson));
+
+                // Comment in to set session id.
+                tagReadMessage.SessionId = sessionId;
+
+                // Send the message
+                await queueClient.SendAsync(tagReadMessage);
+                Console.WriteLine($"Sent: { orderItems[position].Product }");
+
+                Thread.Sleep(100);
+            }
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("{0} total tag reads.", sentCount);
+            Console.WriteLine();
+            Console.ResetColor();
+
+            Console.ReadLine();
+
+        }
+    }
+}
